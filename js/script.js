cf633b91dfff7c6e50f6c632ecfb93dc37941e48
Tiger <tigeryu7@gmail.com>
2018-07-11 16:35:11 -0400

    Updated parallax effect

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
-$('.parallax-window').parallax({
-	imageSrc: 'css/background-image.jpg',
-	zIndex: 2
+var images = ['background-image.jpg', 'background-image2.jpg', 'background-image3.jpg'];
+var path = 'img/'; 
+var n_windows = document.getElementsByClassName("parallax-window").length;
+var n_images = images.length;
+
+$(document).ready(function(n_images){
+	$('.parallax-window').each(function(i){
+		if (i > n_images - 1) {
+			i = Math.round(Math.random(n_images - 1));
+		}
+		$(this).parallax({
+			imageSrc: (path + images[i]),
+			zIndex: 2
+		});
+	});
 });
