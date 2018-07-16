e632f3ee6d2a148fdea644fb1270f653baa2ee91
Tiger <tigeryu7@gmail.com>
2018-07-16 12:02:10 -0400

    added more content and changed styling

--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
+var cover = 'cover-image.jpg';
 var images = ['background-image.jpg', 'background-image2.jpg', 'background-image3.jpg'];
 var path = 'img/'; 
 var n_windows = document.getElementsByClassName("parallax-window").length;
 var n_images = images.length;
 
 $(document).ready(function(n_images){
+	$('.parallax-cover').parallax({
+		imageSrc: (path + cover),
+		zIndex: -5,
+		bleed: 100
+	});
+
 	$('.parallax-window').each(function(i){
 		if (i > n_images - 1) {
 			i = Math.round(Math.random(n_images - 1));
 		}
 		$(this).parallax({
 			imageSrc: (path + images[i]),
-			zIndex: 2
+			zIndex: -1,
+			bleed: 100
 		});
 	});
 });
+
